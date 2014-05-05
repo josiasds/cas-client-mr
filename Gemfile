92d1cccfa07f1eb8545dca140412ae368b3ee801
Josias Schneider <josias.ds@gmail.com>
2014-05-05 17:14:45 -0300

    add gem rack-cas; set cas server-url

--- Gemfile
+++ Gemfile
@@ -27,3 +27,4 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 gem 'rails_12factor'
+gem 'rack-cas'
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module CasClient
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.rack_cas.server_url = 'http://cas-server-mr.herokuapp.com/'
   end
 end
